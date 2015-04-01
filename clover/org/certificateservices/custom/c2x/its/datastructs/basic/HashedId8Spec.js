a5b035f75cd95a306f017399c8995350115fa103
Bamboo User <bamboo@certificateservices.org>
2015-04-01 12:51:39 +0200

    Updating Github Doc

--- clover/org/certificateservices/custom/c2x/its/datastructs/basic/HashedId8Spec.js
+++ clover/org/certificateservices/custom/c2x/its/datastructs/basic/HashedId8Spec.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":44,"id":3815,"methods":[{"el":37,"sc":2,"sl":33},{"el":42,"sc":2,"sl":39}],"name":"HashedId8Spec","sl":31}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_257":{"methods":[{"sl":39}],"name":"Verify toString","pass":true,"statements":[{"sl":41}]},"test_355":{"methods":[{"sl":33}],"name":"Verify the correct octet length of the HashedId8","pass":true,"statements":[{"sl":35}]}}
+clover.testTargets = {"test_315":{"methods":[{"sl":33}],"name":"Verify the correct octet length of the HashedId8","pass":true,"statements":[{"sl":35}]},"test_362":{"methods":[{"sl":39}],"name":"Verify toString","pass":true,"statements":[{"sl":41}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [355], [], [355], [], [], [], [257], [], [257], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [315], [], [315], [], [], [], [362], [], [362], [], [], []]
--- clover/org/certificateservices/custom/c2x/its/datastructs/msg/TrailerFieldTypeSpec.js
+++ clover/org/certificateservices/custom/c2x/its/datastructs/msg/TrailerFieldTypeSpec.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":52,"id":4373,"methods":[{"el":40,"sc":2,"sl":31},{"el":50,"sc":2,"sl":42}],"name":"TrailerFieldTypeSpec","sl":29}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_138":{"methods":[{"sl":31}],"name":"Verify that signature has bytevalue 1","pass":true,"statements":[{"sl":34},{"sl":35}]},"test_373":{"methods":[{"sl":42}],"name":"Verify that TrailerFieldType.getByValue returns signature for 1","pass":true,"statements":[{"sl":45}]}}
+clover.testTargets = {"test_200":{"methods":[{"sl":42}],"name":"Verify that TrailerFieldType.getByValue returns signature for 1","pass":true,"statements":[{"sl":45}]},"test_233":{"methods":[{"sl":31}],"name":"Verify that signature has bytevalue 1","pass":true,"statements":[{"sl":34},{"sl":35}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [138], [], [], [138], [138], [], [], [], [], [], [], [373], [], [], [373], [], [], [], [], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [233], [], [], [233], [233], [], [], [], [], [], [], [200], [], [], [200], [], [], [], [], [], [], []]
