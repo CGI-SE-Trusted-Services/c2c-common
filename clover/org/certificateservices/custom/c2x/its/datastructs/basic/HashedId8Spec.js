580d653709d741d435503e620fff3512335d535a
Bamboo User <bamboo@certificateservices.org>
2015-04-01 13:36:08 +0200

    Updating Github Doc

--- clover/org/certificateservices/custom/c2x/its/datastructs/basic/HashedId8Spec.js
+++ clover/org/certificateservices/custom/c2x/its/datastructs/basic/HashedId8Spec.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":44,"id":3815,"methods":[{"el":37,"sc":2,"sl":33},{"el":42,"sc":2,"sl":39}],"name":"HashedId8Spec","sl":31}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_233":{"methods":[{"sl":39}],"name":"Verify toString","pass":true,"statements":[{"sl":41}]},"test_297":{"methods":[{"sl":33}],"name":"Verify the correct octet length of the HashedId8","pass":true,"statements":[{"sl":35}]}}
+clover.testTargets = {"test_231":{"methods":[{"sl":39}],"name":"Verify toString","pass":true,"statements":[{"sl":41}]},"test_73":{"methods":[{"sl":33}],"name":"Verify the correct octet length of the HashedId8","pass":true,"statements":[{"sl":35}]}}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [297], [], [297], [], [], [], [233], [], [233], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [73], [], [73], [], [], [], [231], [], [231], [], [], []]
